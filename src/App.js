7996e882ece55961b3459fe5533df7b38dcb343e
Sargy <lukina_sargy@mail.ru>
2019-07-11 19:19:56 +0300

    43

--- src/App.js
+++ src/App.js
@@ -1,20 +1,40 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-        <h1>Hello, world!</h1>
-        <Person name={"Max"} age={"28"}/>
-        <Person name={"Stephanie"} age={"26"}/>
-        <Person name={"Manu"} age={"29"}>
-            <p>children text</p>
-            <p>children text</p>
-            <p>children text</p>
-        </Person>
-    </div>
-  );
+class App extends Component {
+    state = {
+        persons: [
+            {name: 'Max', age: 29},
+            {name: 'Stephanie', age: 29},
+            {name: 'Manu', age: 29},
+        ],
+        otherState: 'somevalue'
+    }
+
+    switchNameHandler = () =>   {
+        // console.log('HANDLER ACTIVATED');
+        // this.state.persons[0].name = 'Maximillian';
+        this.setState({
+            persons: [
+            {name: 'Maximillian', age: 29},
+            {name: 'Stephanie', age: 12},
+            {name: 'Manu', age: 29},
+            ]
+        })
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <h1>Hello, world!</h1>
+                <button onClick={this.switchNameHandler}>Switch name</button>
+                <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+                <Person name={this.state.persons[1].name} age={this.state.persons[1].age}/>
+                <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
+            </div>
+        );
+    }
 }
 
 export default App;
