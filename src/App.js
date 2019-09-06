ead4f212c4c2079e27e0fb52915f290cdefc3253
Sargy <lukina_sargy@mail.ru>
2019-09-06 13:44:13 +0300

    66 setting class names

--- src/App.js
+++ src/App.js
@@ -48,12 +48,15 @@ class App extends Component {
 
     render() {
         const style = {
-            backgroundColor: 'skyblue',
+            backgroundColor: 'green',
+            color: 'white',
             font: 'inherit',
             border: '1px solid skyblue',
             padding: '8px',
-            cursor: 'pointer'
-        };
+            cursor: 'pointer',
+            transition: '0.3s all'
+
+    };
         let persons = null;
         if (this.state.showPersons) {
             persons = (
@@ -71,10 +74,20 @@ class App extends Component {
                     })}
                 </div>
             );
+            style.backgroundColor = 'red';
+        }
+
+        const classes = [];
+        if (this.state.persons.length <= 2) {
+            classes.push('red'); //classes = ['red];
+        }
+        if (this.state.persons.length <=1) {
+            classes.push('bold'); //classes = ['red', 'bold']
         }
         return (
             <div className="App">
                 <h1>Hello, world!</h1>
+                <p className={classes.join(' ')}>This is really working!</p>
                 <button style={style}
                         onClick={this.togglePersonsHandler}>Toggle Persons
                 </button>
