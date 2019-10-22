ac4966a0133f3a1fe526bf2cad9aae896bbb773f
Sargy <lukina_sargy@mail.ru>
2019-10-22 18:20:34 +0300

    89 component lifecycle

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,6 +4,11 @@ import Persons from '../components/Persons/Persons';
 import Cockpit from "../components/Cockpit/Cockpit";
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        console.log("[App.js] constructor");
+    }
+
     state = {
         persons: [
             {id: 'qwde', name: 'Max', age: 29},
@@ -14,6 +19,14 @@ class App extends Component {
         showPersons: false
     };
 
+    static getDerivedStateFromProps(props, state) {
+        console.log("[App.js] getDerivedStateFromProps", props);
+        return state;
+    };
+    componentDidMount() {
+        console.log("[App.js] componentDidMount");
+
+    };
 
     nameChangedHandler = (event, id) => {
         const personIndex = this.state.persons.findIndex(pers => {
@@ -46,6 +59,7 @@ class App extends Component {
     };
 
     render() {
+        console.log('[App.js] render');
         let persons = null;
         if (this.state.showPersons) {
             persons =  <Persons
