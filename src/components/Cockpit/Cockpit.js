0cf207755aacd966dc7aa2e537133509e21bd59e
Sargy <lukina_sargy@mail.ru>
2019-10-24 18:04:53 +0300

    93 useeffect

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import styles from "./Cockpit.module.css";
 
 const Cockpit = (props) => {
+    useEffect(() => {
+        console.log('[Cockpit.js] useEffect');
+    });
+
     const assignedClasses = [];
     let btnClass='';
     if (props.show) {
