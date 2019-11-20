957cae911672c4a6fe6dbffa0b857c044766ee96
Sargy <lukina_sargy@mail.ru>
2019-11-20 17:47:42 +0300

    higher order component start

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import Aux from '../../../hoc/Auxiliary';
 import classes from './Person.module.css';
 
 
@@ -6,11 +7,11 @@ class Person extends Component{
     render  () {
         console.log('[Person.js] rendering...');
         return (
-            <div className={classes.Person}>
+            <Aux>
                 <p onClick={this.props.click}>I'm {this.props.name} and I'm {this.props.age} years old!</p>
                 <div>{this.props.children}</div>
                 <input type="text"  onChange={this.props.changed} value={this.props.name}/>
-            </div>
+            </Aux>
         )
     };
     }
